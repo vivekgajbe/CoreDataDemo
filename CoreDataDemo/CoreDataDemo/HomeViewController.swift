02ba7c0e57a694370265a226a55969b3e4d31d49
vivek <vpgajbe1991@gmail.com>
2017-11-10 12:53:30 +0530

    Save user details to coredata. Delete user record from coredata Worked on form UI Add camera and gallery on click of profile image

---
+++ CoreDataDemo/CoreDataDemo/HomeViewController.swift
@@ -0,0 +1,432 @@
+//
+//  HomeViewController.swift
+//  CoreDataDemo
+//
+//  Created by vivek gajbe on 09/11/17.
+//  Copyright © 2017 Vivek Gajbe. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class HomeViewController: UIViewController,UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITextFieldDelegate {
+
+    //MARK:- Variable declaration
+    let imgPicker = UIImagePickerController()
+    var userDetails: [NSManagedObject] = []
+    var iKeybordHeight = Int()
+    var strProfileUrl = String()
+    //MARK:- Outlet declaration
+    
+    @IBOutlet var imgProfile: UIImageView!
+    @IBOutlet var txtFName: UITextField!
+    @IBOutlet var txtLName: UITextField!
+    @IBOutlet var txtGender: UITextField!
+    @IBOutlet var txtCity: UITextField!
+    @IBOutlet var txtEmail: UITextField!
+    @IBOutlet var txtMobileNumber: UITextField!
+    @IBOutlet var txtPincode: UITextField!
+    @IBOutlet var txtOccupation: UITextField!
+    @IBOutlet var txtPassport: UITextField!
+    @IBOutlet var txtAadhar: UITextField!
+    
+    
+    @IBOutlet var btnSave: UIButton!
+    @IBOutlet var btnUpdate: UIButton!
+    @IBOutlet var btnDelete: UIButton!
+    
+    @IBOutlet var viwContainer: UIView!
+    @IBOutlet var scrlViwHome: UIScrollView!
+    
+    // MARK: - View life cycle
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        imgPicker.delegate = self
+
+        btnSave.layer.borderWidth = 1.0
+        btnUpdate.layer.borderWidth = 1.0
+        btnDelete.layer.borderWidth = 1.0
+        
+        btnSave.layer.cornerRadius = 15
+        btnUpdate.layer.cornerRadius = 15
+        btnDelete.layer.cornerRadius = 15
+        
+        
+        self.txtFName.delegate = self
+        self.txtLName.delegate = self
+        self.txtGender.delegate = self
+        self.txtCity.delegate = self
+        self.txtEmail.delegate = self
+        self.txtMobileNumber.delegate = self
+        self.txtPincode.delegate = self
+        self.txtMobileNumber.delegate = self
+        
+        self.getUserDetails()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        
+        
+        
+        //add tap gesture to hide keyboard
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.resignKeyboard))
+        viwContainer.addGestureRecognizer(tapGesture)
+        
+        
+        
+    }
+    func resignKeyboard()
+    {
+        scrlViwHome.contentSize = CGSize(width: scrlViwHome.frame.size.width, height: viwContainer.frame.size.height )
+        txtFName.resignFirstResponder()
+        txtLName.resignFirstResponder()
+        txtGender.resignFirstResponder()
+        txtCity.resignFirstResponder()
+        txtEmail.resignFirstResponder()
+        txtMobileNumber.resignFirstResponder()
+        txtPincode.resignFirstResponder()
+        txtOccupation.resignFirstResponder()
+        txtPassport.resignFirstResponder()
+        txtAadhar.resignFirstResponder()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - user define method 
+    func getUserDetails()
+    {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            let fetchRequest = NSFetchRequest<ProfileDetails>(entityName: "ProfileDetails")
+            let _ : NSFetchRequest<ProfileDetails> = ProfileDetails.fetchRequest()
+            //fetchRequest.predicate = NSPredicate(format: "uniqueId == %@", contactIdentifier)
+            let fetchedResults = try context.fetch(fetchRequest) 
+            if let aContact = fetchedResults.first
+            {
+                let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                
+                let fileURL : String = "\(documentsURL)\(aContact.imgProfile!)"
+                var bIsFileExist = Bool()
+                    
+                   bIsFileExist = FileManager.default.fileExists(atPath: fileURL)
+                
+                if (bIsFileExist)
+                {
+                    imgProfile.image    = UIImage(contentsOfFile: fileURL)
+                }
+
+                txtFName.text = aContact.fName
+                txtLName.text = aContact.lName
+                txtGender.text = aContact.gender
+                txtCity.text = aContact.city
+                txtEmail.text = aContact.email
+                txtMobileNumber.text = aContact.mobileNumber
+                txtPincode.text = aContact.lName
+                txtOccupation.text = aContact.occupation
+                txtAadhar.text = aContact.aadhar
+                txtPassport.text = aContact.passport
+                
+            }
+        }
+        catch {
+            print ("fetch task failed", error)
+        }
+        
+        
+        
+        
+        
+        
+    }
+    func fetchRequest() -> NSFetchRequest<ProfileDetails>
+    {
+        return NSFetchRequest<ProfileDetails>(entityName: "ProfileDetails")
+    }
+    @IBAction func saveUserForm(_ sender: Any)
+    {
+        if self.validate()
+        {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            
+            if #available(iOS 10.0, *) {
+                let managedContext = appDelegate.persistentContainer.viewContext
+                let entity = NSEntityDescription.entity(forEntityName: "ProfileDetails",
+                                                        in: managedContext)!
+                
+                let person = NSManagedObject(entity: entity,
+                                             insertInto: managedContext)
+                person.setValue(strProfileUrl, forKey: "imgProfile")
+                person.setValue(txtFName.text, forKeyPath: "fName")
+                person.setValue(txtFName.text, forKeyPath: "fName")
+                person.setValue(txtLName.text, forKeyPath: "lName")
+                person.setValue(txtGender.text, forKeyPath: "gender")
+                person.setValue(txtCity.text, forKeyPath: "city")
+                person.setValue(txtEmail.text, forKeyPath: "email")
+                person.setValue(txtMobileNumber.text, forKeyPath: "mobileNumber")
+                person.setValue(txtPincode.text, forKeyPath: "pincode")
+                person.setValue(txtOccupation.text, forKeyPath: "occupation")
+                person.setValue(txtPassport.text, forKeyPath: "passport")
+                person.setValue(txtAadhar.text, forKeyPath: "aadhar")
+                
+                do {
+                    try managedContext.save()
+                    userDetails.append(person)
+                    self.showAlert(strMessage: "User details save successfully")
+                    self.disableTextFeildAction()
+                } catch let error as NSError {
+                    print("Could not save. \(error), \(error.userInfo)")
+                }
+                
+            }
+            else
+            {
+                // Fallback on earlier versions
+            }
+        }
+        
+    }
+    func disableTextFeildAction()
+    {
+        self.txtFName.isUserInteractionEnabled = false
+        self.txtLName.isUserInteractionEnabled = false
+        self.txtGender.isUserInteractionEnabled = false
+        self.txtCity.isUserInteractionEnabled = false
+        self.txtEmail.isUserInteractionEnabled = false
+        self.txtMobileNumber.isUserInteractionEnabled = false
+        self.txtPincode.isUserInteractionEnabled = false
+        self.txtMobileNumber.isUserInteractionEnabled = false
+        
+    }
+    func enableTextFeildAction()
+    {
+        self.txtFName.isUserInteractionEnabled = true
+        self.txtLName.isUserInteractionEnabled = true
+        self.txtGender.isUserInteractionEnabled = true
+        self.txtCity.isUserInteractionEnabled = true
+        self.txtEmail.isUserInteractionEnabled = true
+        self.txtMobileNumber.isUserInteractionEnabled = true
+        self.txtPincode.isUserInteractionEnabled = true
+        self.txtMobileNumber.isUserInteractionEnabled = true
+        
+    }
+    func setEmptyToTextFeild()
+    {
+        self.txtFName.text = ""
+        self.txtLName.text = ""
+        self.txtGender.text = ""
+        self.txtCity.text = ""
+        self.txtEmail.text = ""
+        self.txtMobileNumber.text = ""
+        self.txtPincode.text = ""
+        self.txtMobileNumber.text = ""
+        self.txtOccupation.text = ""
+        self.txtAadhar.text = ""
+        self.txtPassport.text = ""
+        
+    }
+    func validate() -> Bool
+    {
+        if imgProfile.image == UIImage(named: "Profile")
+        {
+            self.showAlert(strMessage: "Please select Profile Picture")
+            return false
+        }
+        else
+        if let text = txtFName.text, text.isEmpty
+        {
+           self.showAlert(strMessage: "Please enter First Name")
+            return false
+        }
+        else
+        if let text = txtLName.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter Last Name")
+            return false
+        }
+        else
+        if let text = txtGender.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter Gender")
+            return false
+        }
+        else
+        if let text = txtCity.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter City")
+            return false
+        }
+        else
+        if let text = txtEmail.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter email")
+            return false
+        }
+        else
+        if let text = txtMobileNumber.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter Mobile Number")
+            return false
+        }
+        else
+        if let text = txtPincode.text, text.isEmpty
+        {
+            self.showAlert(strMessage: "Please enter pin code")
+            return false
+        }
+        else
+        if let text = txtEmail.text
+        {
+            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            
+            let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+            if emailTest.evaluate(with: text) == false
+            {
+                self.showAlert(strMessage: "Please enter valid email")
+                return false
+            }
+        }
+        
+        return true
+    }
+    func showAlert(strMessage:String)
+    {
+        let alert = UIAlertController(title: "Alert", message: strMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    func openCamera()
+    {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            imgPicker.sourceType = .camera
+            self.present(imgPicker, animated: true, completion: nil)
+        }
+        else
+        {
+            self.showAlert(strMessage: "You don't have camera")
+        }
+    }
+    func openGallary()
+    {
+        imgPicker.allowsEditing = false
+        imgPicker.sourceType = .photoLibrary
+        self.present(imgPicker, animated: true, completion: nil)
+    }
+       func SavePofileImage(imgProfile:UIImageView)
+    {
+        do {
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let strProfileName = "profile.png"
+            strProfileUrl = strProfileName
+            let fileURL = documentsURL.appendingPathComponent(strProfileName)
+            
+            if let pngImageData = UIImagePNGRepresentation(imgProfile.image!)
+            {
+                try pngImageData.write(to: fileURL, options: .atomic)
+            }
+        }
+        catch
+        {
+        }
+    }
+    //MARK: - Image delegate method
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    //@nonobjc func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    
+    {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imgProfile.contentMode = .scaleAspectFill
+            imgProfile.image = pickedImage
+            
+            imgProfile.layer.cornerRadius = imgProfile.frame.size.height / 2
+
+            imgProfile.clipsToBounds = true
+            self.SavePofileImage(imgProfile: imgProfile)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+    {
+    dismiss(animated: true, completion: nil)
+    }
+    // MARK: - Button Delegate method
+    @IBAction func btnSelectProfilePic(_ sender: Any)
+    {
+        let actionSheet = UIAlertController.init(title: "Please choose a source type", message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction.init(title: "Take Photo", style: .default, handler: { (action) in
+            print("camera")
+            self.openCamera()
+        }))
+        actionSheet.addAction(UIAlertAction.init(title: "Choose Photo", style: UIAlertActionStyle.default, handler: { (action) in
+            print("gallery")
+            self.openGallary()
+        }))
+        actionSheet.addAction(UIAlertAction.init(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) in
+            // self.dismissViewControllerAnimated(true, completion: nil) is not needed, this is handled automatically,
+            //Plus whatever method you define here, gets called,
+            //If you tap outside the UIAlertController action buttons area, then also this handler gets called.
+        }))
+        //Present the controller
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+
+    @IBAction func btnUpdateClick(_ sender: Any)
+    {
+        self.enableTextFeildAction()
+        self.showAlert(strMessage: "User details allow to update")
+        //self.saveUserForm(btnSave)
+    }
+    @IBAction func btnDeleteClick(_ sender: Any)
+    {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        if #available(iOS 10.0, *) {
+            let managedContext = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<ProfileDetails>(entityName: "ProfileDetails")
+            
+               // try managedContext.save()
+                if let result = try? managedContext.fetch(fetchRequest) {
+                    for object in result {
+                        managedContext.delete(object)
+                        self.setEmptyToTextFeild()
+                        self.showAlert(strMessage: "User details delete successfully")
+                    }
+                }
+        }
+    }
+    
+    //MARK:- text feild delegate method
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("TextField did begin editing method called")
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
+        textField.resignFirstResponder()
+        scrlViwHome.contentSize = CGSize(width: scrlViwHome.frame.size.width, height: viwContainer.frame.size.height )
+        return true
+    }
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardSize.height
+            scrlViwHome.contentSize = CGSize(width: scrlViwHome.frame.size.width, height: viwContainer.frame.size.height + keyboardHeight)
+            iKeybordHeight = Int(keyboardHeight)
+            
+        }
+    }
+    
+
+}
